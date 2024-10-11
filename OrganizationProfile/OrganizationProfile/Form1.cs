6557b204214e1214e890120d6c81e47655feb820
elijahreinsss <146070509+elijahreinsss@users.noreply.github.com>
2024-10-11 18:11:06 +0800

    Add files via upload

---
+++ OrganizationProfile/OrganizationProfile/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace OrganizationProfile
+{
+    public partial class frmRegistration : Form
+    {
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+
+        private void frmRegistration_Load(object sender, EventArgs e)
+     
+                {
+
+            string[] Gender = new string[] {
+            "Male",
+            "Female",
+
+            };
+            for (int i = 0; i < 2; i++)
+            {
+
+                cbGender.Items.Add(Gender[i].ToString());
+            }
+
+
+            string[] ListOfProgram = new string[]{
+ "BS Information Technology",
+ "BS Computer Science",
+ "BS Information Systems",
+ "BS in Accountancy",
+ "BS in Hospitality Management",
+ "BS in Tourism Management"
+ };
+ for (int i = 0; i < 6; i++) 
+            {
+                cbProgram.Items.Add(ListOfProgram[i].ToString());
+            }
+        }
+        public long StudentNumber(string studNum)
+        {
+
+            _StudentNo = long.Parse(studNum);
+
+            return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+            if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+            {
+                _ContactNo = long.Parse(Contact);
+            }
+
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+            }
+
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+
+            return _Age;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            StudentInformationClass.SetFullName = FullName(txtLastName.Text,
+            txtFirstName.Text, txtMiddleInitial.Text);
+            StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
+            StudentInformationClass.SetProgram = cbProgram.Text;
+            StudentInformationClass.SetGender = cbGender.Text;
+            StudentInformationClass.SetContactNo = ContactNo(text1.Text);
+            StudentInformationClass.SetAge = Age(txtAge.Text);
+            StudentInformationClass.SetBirthday = datePickerBirthday.Value.ToString("yyyyMM-dd");
+           
+            Form2 frm = new Form2();
+            
+            frm.ShowDialog();
+        }
+
+        private void cbGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+
+        }
+    }
+}
+
---
+++ OrganizationProfile/OrganizationProfile/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrganizationProfile
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            lblStudentNo.Text = StudentInformationClass.SetStudentNo.ToString();
+            lblName.Text = StudentInformationClass.SetFullName;
+            lblProgram.Text = StudentInformationClass.SetProgram;
+            lblBirthday.Text = StudentInformationClass.SetBirthday;
+            lblGender.Text = StudentInformationClass.SetGender;
+            lblAge.Text = StudentInformationClass.SetAge.ToString();
+            lblContactNo.Text = StudentInformationClass.SetContactNo.ToString();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
